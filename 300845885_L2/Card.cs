948fd339fb0545d9baae1a3ef1818d9bb1f37375
kferna34 <kferna34@my.centennialcollege.ca>
2016-06-07 22:12:16 -0400

    Add project files.

---
+++ 300845885_L2/Card.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _300845885_L2
+{
+    /// <summary>
+    /// This class creates an object that represents a playing  card
+    /// </summary>
+    /// @class card
+    public class Card
+    {
+
+
+        //public prop +++++++++++++++++++++++++++++++++++++++++++
+        public int Face { get; set; }
+        public string Suit { get; set; }
+
+        /// <summary>
+        /// this constructor takes 2 param: face & suit to instantiate a card object
+        /// </summary>
+        /// @constructor Card
+        /// @param {string} face
+        /// @param{string} suit
+        public Card(int face, string suit)
+        {
+            this.Face = face;
+            this.Suit = suit;
+
+        }
+
+    }
+}
---
+++ 300845885_L2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _300845885_L2
+{
+    /**
+     * This class is the "driver" class for our Program
+     * 
+     * @class Program
+     */
+    public class Program
+    {
+        /**
+         * the main method for our driver class Program
+         * 
+         * @method Main
+         * @param {sting[]} args
+         * @returns {void}
+         */
+        public static void Main(string[] args)
+        {
+            Deck deck = new Deck();
+            deck.Display();
+            deck.ShuffleDeck();
+            deck.Display();
+            Card cardDelt = deck.Deal();
+            Console.WriteLine("Card Delt :{0} of {1}", cardDelt.Face, cardDelt.Suit);
+            Console.WriteLine();
+            deck.Deal();
+
+        }
+
+    }
+
+}
+
